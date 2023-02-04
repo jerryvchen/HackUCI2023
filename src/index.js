04e1a0099d48cb27ec9f040a864801b50879e821
Kelvin Zhao <yunxi.kelvin@gmail.com>
2023-02-04 12:31:09 -0800

    Created Multi-Page Design
    There is now an navbar and multiple pages to which you can navigate to.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Landing from "./pages/Landing";
+import Planner from "./pages/Planner";
+import NoPage from "./pages/NoPage";
+import './App.css';
 
-import App from './App';
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="planner" element={<Planner />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
