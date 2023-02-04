73c67a0eb2b462e6e464b9b690493b9ecdc26468
Jerry Chen <69334968+jerryvchen@users.noreply.github.com>
2023-02-04 12:55:08 -0800

    Finished first version of planner.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,9 @@
 import planner
 
 def main() -> None:
-    pa
+    input = planner.read_input_file_path()
+    planner_dict = planner.create_planner_from_path(input)
+    planner.print_planner(planner_dict)
 
 if __name__ == '__main__':
     main()
--- scripts/planner.py
+++ scripts/planner.py
@@ -1,35 +1,54 @@
 # planner.py
 
 from pathlib import Path
-def planner(start_day: int, end_day: int, tasks: list[str]) -> dict[int, list[str]]:
+def create_planner(start_day: int, end_day: int, tasks: list[str]) -> dict[int, list[str]]:
     """Divides up the tasks into a certain number of days, and prints it out to an external file. """
+    planner = dict()
     total_days = end_day - start_day + 1 # including today
-    
+    for day in range(start_day, total_days + 1):
+        planner[day] = list()
+    tasks_per_day = len(tasks) // total_days
+    extra_tasks = len(tasks) % total_days
+
+    list_index = 0
+    for day in range(start_day, total_days + 1):
+        for _ in range(tasks_per_day):
+            planner[day].append(tasks[list_index])
+            list_index += 1
+        if extra_tasks > 0:
+            planner[day].append(tasks[list_index])
+            list_index += 1
+            extra_tasks -= 1
+    return planner
 
 
 
-
-
-def input_tasks(path: Path) -> list[str]:
-    """Parses through a file and returns a compiled list of tasks."""
+def create_planner_from_path(path: Path) -> dict[int, list[str]]:
+    """Parses through a file and returns a planner dict."""
     tasks = list()
     try:
         with open(path, 'r') as file:
             start_day = int(file.readline())
-            
+            end_day = int(file.readline())
+            tasks = list()
+            for line in file:
+                tasks.append(line.strip())
+            planner = create_planner(start_day, end_day, tasks)
             print('SUCCESS')
+            return planner
     except FileNotFoundError:
         print('FILE NOT FOUND')
+    
 
-def _read_input_file_path() -> Path:
+def read_input_file_path() -> Path:
     """Reads the input file path from standard input"""
     return Path(input())
 
 
-def _print_planner(planner: dict[int, list[str]]):
+def print_planner(planner: dict[int, list[str]]):
     """Prints the planner into the console."""
-    for day, events in planner:
-        event_str = ', '.join(events)
+    for day in planner.keys():
+        event_str = ', '.join(planner[day])
         print('Day ' + str(day) + ': ' + event_str)
 
 
