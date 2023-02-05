59dc429357a6e77a500d21be29512ce4b6bf02c6
Jerry Chen <69334968+jerryvchen@users.noreply.github.com>
2023-02-04 17:48:54 -0800

    created flask_test and related files

--- backend/scripts/flask_test.py
+++ backend/scripts/flask_test.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS # remove later
+from flask_testApiHandler import flask_testApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='')
+CORS(app) # also remove later
+api = Api(app)
 
-@app.route('/')
+@app.route('/', defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 def hello():
     return 'Hello World!'
 
