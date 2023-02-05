342db87caf44e1f28d6620ea738f4308e82be865
dopherdo <82693367+dopherdo@users.noreply.github.com>
2023-02-04 16:25:28 -0800

    added progress bar again

--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import ProgressBar from "../components/progress-bar"
+import { useState, useEffect} from 'react';
+
+const testData = [{ bgcolor: "#6a1b9a", completed: 69 }];
 
 function Planner() {
     return (
-        <h1>this is the planner</h1>
-        
+        <div className="App">
+            <h1>this is the planner</h1>
+             {testData.map((item, idx) => (
+        <ProgressBar
+          key={idx}
+          bgcolor={item.bgcolor}
+          completed={item.completed}
+        />
+      ))}
+    </div>
     );
 }
-
 export default Planner;
